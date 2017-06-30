6ea6517c6f37e5a7201ee9d14b3c5aaaee4a9081
Jeongsoo Park <toracle@gmail.com>
2017-06-30 13:38:25 +0900

    update

--- Gemfile
+++ Gemfile
@@ -4,3 +4,6 @@ gem 'middleman', '~> 4.2'
 gem 'middleman-autoprefixer', '~> 2.7'
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
+
+# Added at 2017-06-29 23:02:42 +0900 by toracle:
+gem "pandoc-ruby", "~> 2.0"
--- config.rb
+++ config.rb
@@ -13,6 +13,10 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+page '/documentation.html', layout: 'docs'
+
+activate :directory_indexes
+
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 
